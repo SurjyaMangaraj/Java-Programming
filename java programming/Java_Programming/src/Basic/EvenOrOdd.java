506f28c09b27a16128ce67302f8e2bfe2bf9ba94
Surjya Prakash Mangaraj <77107712+SurjyaMangaraj@users.noreply.github.com>
2025-12-31 09:56:58 +0530

    Update prompt message for even or odd check

--- java programming/Java_Programming/src/Basic/EvenOrOdd.java
+++ java programming/Java_Programming/src/Basic/EvenOrOdd.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class EvenOrOdd {
     public static void main(String []args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter number to Check.");
+        System.out.println("Enter number to Check weather it is even or odd.");
         int n=sc.nextInt();
         if(n%2==0){
             System.out.println(n+" is the Even Number.");
